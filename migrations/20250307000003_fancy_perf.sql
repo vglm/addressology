7f7ddcf984322e2c944b6741707e4515230f7924
scx1332 <sieciech@gmail.com>
2025-09-18 19:20:07 +0200

    Added performance indices

--- migrations/20250307000003_fancy_perf.sql
+++ migrations/20250307000003_fancy_perf.sql
@@ -1,11 +1,11 @@
 DROP INDEX IF EXISTS fancy_factory_idx;
 DROP INDEX IF EXISTS fancy_score_idx;
 DROP INDEX IF EXISTS public_key_base_idx;
-DROP INDEX IF EXISTS job_info_miner_idx;
+DROP INDEX IF EXISTS miner_prov_node_id_idx;
 
 CREATE INDEX fancy_factory_idx ON fancy (factory);
 CREATE INDEX fancy_score_idx ON fancy (score);
 CREATE INDEX public_key_base_idx ON public_key_base (hex);
-CREATE INDEX job_info_miner_idx ON job_info (miner);
+CREATE INDEX miner_prov_node_id_idx ON miner_info (prov_node_id);
 
 
